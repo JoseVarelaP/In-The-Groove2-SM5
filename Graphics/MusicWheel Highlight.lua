10a49699c16762499c1e61823822075dc8af6bb0
Jose_Varela <jo.lu6853@gmail.com>
2018-05-15 10:50:01 -0500

    Music wheel perfected (around 95%)

--- Graphics/MusicWheel Highlight.lua
+++ Graphics/MusicWheel Highlight.lua
@@ -1,5 +1,4 @@
 return Def.ActorFrame{
 	InitCommand=cmd(fov,58;ztest,1;addx,-500;sleep,0.4;linear,0.45;addx,500);
-	--OffCommand=cmd(linear,0.5;addx,-420);
-	LoadActor("WheelItems/Wheel highlight")..{ OnCommand=cmd(x,-80;zoomx,1.2;rotationz,0.5;rotationy,-5;diffuseshift;effectcolor2,1,1,1,0.7;effectcolor1,1,1,1,0.3;effectperiod,1.0;effectoffset,0.2;effectclock,"beat";ztest,1) },
+	LoadActor("WheelItems/Wheel highlight")..{ OnCommand=cmd(x,-80;zoomx,1.3;zoomy,1.4;y,-6;diffuseshift;effectcolor2,1,1,1,0.7;effectcolor1,1,1,1,0.3;effectperiod,1.0;effectoffset,0.2;effectclock,"beat";ztest,1) },
 }
