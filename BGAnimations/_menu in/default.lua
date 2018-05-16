78d5d5367b2d3a9a0760b6728ad94ca33b8db7c5
ListenerJubatus <shawn.thompson306@gmail.com>
2018-05-16 14:33:58 -0600

    trying to add the transitions

--- BGAnimations/_menu in/default.lua
+++ BGAnimations/_menu in/default.lua
@@ -1,5 +1,5 @@
 return Def.ActorFrame{
 	LoadActor("../_moveon")..{
-	OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;linear,0.2;diffusealpha,0);
+	StartTransitioningCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;linear,0.2;diffusealpha,0);
 	},
 }
