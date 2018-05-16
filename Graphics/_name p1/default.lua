19f6e70c2f8fafb05c9f306246885cb19769a5b7
Jose_Varela <jo.lu6853@gmail.com>
2018-05-16 17:00:25 -0500

    MadkaT: PlayerOptions icons fix

--- Graphics/_name p1/default.lua
+++ Graphics/_name p1/default.lua
@@ -1,11 +1,14 @@
 return Def.ActorFrame{
 	LoadActor( THEME:GetPathG("","_name frame") ),
-	
 	Def.BitmapText{
-	Text=GAMESTATE:GetPlayerDisplayName(PLAYER_1);
 	Font="_eurostile normal",
-	OnCommand=cmd(shadowlength,2;zoom,0.5;x,-16;y,-2;diffuse,PlayerColor(PLAYER_1)); 
+	OnCommand=function(self)
+		self:shadowlength(2):zoom(0.5):x(-16):y(-2)
+		if GAMESTATE:IsSideJoined(PLAYER_1) then
+			self:diffuse(PlayerColor(PLAYER_1))
+			self:settext(GAMESTATE:GetPlayerDisplayName(PLAYER_1))
+		end
+	end
 	},
-
 	LoadActor( "icon p1" )..{ OnCommand=cmd(shadowlength,2;x,44;y,-4); },
 }
--- Graphics/_name p2/default.lua
+++ Graphics/_name p2/default.lua
@@ -1,9 +1,14 @@
 return Def.ActorFrame{
 	LoadActor( THEME:GetPathG("","_name frame") ),
-	
-	Def.BitmapText{ Text=GAMESTATE:GetPlayerDisplayName(PLAYER_2); Font="_eurostile normal",
-		OnCommand=cmd(shadowlength,2;zoom,0.5;x,-16;y,-2;diffuse,PlayerColor(PLAYER_2)); 
+	Def.BitmapText{
+	Font="_eurostile normal",
+	OnCommand=function(self)
+		self:shadowlength(2):zoom(0.5):x(-16):y(-2)
+		if GAMESTATE:IsSideJoined(PLAYER_2) then
+			self:diffuse(PlayerColor(PLAYER_2))
+			self:settext(GAMESTATE:GetPlayerDisplayName(PLAYER_2))
+		end
+	end
 	},
-
 	LoadActor( "icon p2" )..{ OnCommand=cmd(shadowlength,2;x,44;y,2); },
 }
