f07e73300fae854b51ac1ab07d34bb2c1bf17f37
Jose_Varela <jo.lu6853@gmail.com>
2018-05-28 22:42:00 -0500

    Fixed some Grade Tiers

--- Graphics/_grade models/Grade_Tier11.lua
+++ Graphics/_grade models/Grade_Tier11.lua
@@ -1,5 +1,10 @@
 return Def.ActorFrame{
 	OnCommand=cmd(zoom,0.7;rotationy,-90;spin;effectmagnitude,0,40,0);
 	LoadActor("b.txt")..{ OnCommand=cmd(x,-20) },
-	LoadActor("b plus.model")..{ OnCommand=cmd(x,35) },
+	Def.Model{
+		Meshes="plus.txt",
+		Materials="b.txt",
+		Bones="plus.txt",
+		OnCommand=cmd(x,35)
+	},
 }
