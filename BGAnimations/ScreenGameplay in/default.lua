84393d82235d9c58b1f0a0dcf5515c2ae3da5099
Jose_Varela <jo.lu6853@gmail.com>
2019-08-17 00:41:53 -0500

    Fix: FinalStage in Gameplay was looking for BGAnimations for some reason

--- BGAnimations/ScreenGameplay in/default.lua
+++ BGAnimations/ScreenGameplay in/default.lua
@@ -6,7 +6,7 @@ t[#t+1] = Def.ActorFrame{
 		Texture=THEME:GetPathG( "StageAndCourses/ScreenGameplay","stage ".. ToEnumShortString(GAMESTATE:GetCurrentStage()) ),
 		OnCommand=function(self)
 			if GAMESTATE:GetCurrentStage() == "Stage_Final" then
-				self:Load( THEME:GetPathB("StageAndCourses/ScreenGameplay stage","final") )
+				self:Load( THEME:GetPathG("StageAndCourses/ScreenGameplay stage","final") )
 			end
 			self:Center():draworder(105):zoom(1):sleep(1.2):linear(0.3):zoom(0.25):y(SCREEN_BOTTOM-40)
 		end;
