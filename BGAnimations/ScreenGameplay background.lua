28512850a585bc6b4b69e1122ddaa7d642ab1c33
Jose_Varela <jo.lu6853@gmail.com>
2019-09-01 00:34:34 -0500

    Make Fallback BG not appear on songs that include BG changes

--- BGAnimations/ScreenGameplay background.lua
+++ BGAnimations/ScreenGameplay background.lua
@@ -1,6 +1,7 @@
 local t = Def.ActorFrame{}
 
 t[#t+1] = Def.Sprite{
+	Condition=not GAMESTATE:GetCurrentSong():HasBGChanges(),
 	Texture=ThemePrefs.Get("ITG1") and THEME:GetPathG("ITG1/Common fallback","background") or THEME:GetPathG("ITG2 Common fallback","background"),
 	OnCommand=function(s)
 		s:scale_or_crop_background()
